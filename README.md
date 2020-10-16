f91cc13574da32cd86da2b767bcd6702d13e3d68
Ali Baker <alivxlive@gmail.com>
2020-10-17 00:46:49 +0300

    read sections file dynamically

--- README.md
+++ README.md
@@ -13,10 +13,13 @@ This tool use jinja2 template to render it to ready Revealjs html using python.
 1. Add flask to serve the output file.
 2. Add remote API for control the presentation via API.
 3. Add more option to `generate.py`
-4. Simplify the configuration.
 5. add raspberry pi gpio as a presentation control.
 6. Remove static jinja2 variable from the template.
 7. add more examples.
+8. convert ppt to revealjs website.
+9. convert pdf to revealjs website.
+10. convert markdown to revealjs website.
+11. walk through media dir and create revealjs website.
 
 # config.yaml
 Presentation behaviour can be fine-tuned using a wide array of configuration options
--- generate.py
+++ generate.py
@@ -7,25 +7,36 @@ from jinja2 import Template, Environment, FileSystemLoader
 import yaml
 
 
-combinedHTMLSlides = "mergedHTML.html"
+# Load configration
+with open('config.yaml') as configFile:
+    config = yaml.load(configFile, Loader=yaml.FullLoader)
 
 
 def mergeSections():
-    with open(combinedHTMLSlides, 'wb') as wfd:
-        for f in glob.glob('sections/slide*.html'):
+    listOfSection = glob.glob(config['slidesPrefixPathnName'])
+    listOfSection.sort()
+    with open(config['combinedHTMLSlides'], 'wb') as wfd:
+        for f in listOfSection:
+            print(f)
             with open(f, 'rb') as fd:
                 shutil.copyfileobj(fd, wfd)
-    return combinedHTMLSlides
+    return config['combinedHTMLSlides']
 
 
-with open('config.yaml') as f:
-    data = yaml.load(f, Loader=yaml.FullLoader)
+# Combine all section into one html file
+mergeSections()
+
+# Load Template file
 root = os.path.dirname(os.path.abspath(__file__))
 templates_dir = os.path.join(root)
 env = Environment(loader=FileSystemLoader(templates_dir))
-template = env.get_template('mainTemplate.html')
+template = env.get_template(config['templateFileName'])
+outputIndexFile = os.path.join(root, config['htmlOutputFileName'])
+# Save index file
+with open(outputIndexFile, 'w') as fh:
+    fh.write(template.render(config))
 
 
-filename = os.path.join(root, 'index.html')
-with open(filename, 'w') as fh:
-    fh.write(template.render(data))
+#Do a Cleanup
+if os.path.exists(config['combinedHTMLSlides']):
+    os.remove(config['combinedHTMLSlides'])
